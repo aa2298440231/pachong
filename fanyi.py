9e4e80f016c066b8cad1cc7b17853dda13a9c132
Administrator <2298440231@qq.com>
2019-04-19 23:31:16 +0800

    未完成 两个建值影响翻译内容

--- fanyi.py
+++ fanyi.py
@@ -2,24 +2,44 @@ import urllib.request
 import json
 import urllib.parse
 import ssl
+import pprint
 ssl._create_default_https_context=ssl._create_unverified_context
-url="https://fanyi.baidu.com/v2transapi"
+url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
 def get_post(str):
     post={
+        'i':str,
         'from':"en",
-        'to':"zh",
-        'query':str,
-        'transtype':"realtime",
-        'simple_means_flag':"3",
-        'sign':"503011.217042",
-        'token':"80945ac459c14886663cf8fc53617096",
+        'to':"zh - CHS",
+        'smartresult':"dict",
+        'client':"fanyideskweb",
+        'salt':"15556871593500",#emmm
+        'sign':"10c32e10790706905bec2fe52eb36902",#weizhi  978456eb55df87ee03fb408f816633f5
+        'ts':"1555687135134",
+        'bv':"db6b9693e24ea152843dd96fcd289f0f",
+        'doctype':"json",
+        'version':"2.1",
+        'keyfrom':"fanyi.web",
+        'action':"FY_BY_CLICKBUTTION",
     }
     return post
 heades_data={
-'Cookie':"BAIDUID=B652BA03057135AC00BA9ABCFEA1A995:FG=1; BIDUPSID=B652BA03057135AC00BA9ABCFEA1A995; PSTM=1554443175; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; BDUSS=HBFQngzdGF1NXV3VVZBTXI4Zk1BUy05T3FxSEJGTDE5M0UyQ2JsS3hnYzdnTlZjRVFBQUFBJCQAAAAAAAAAAAEAAAC1WWvzZmx3bnh1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADvzrVw7861cV; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; locale=zh; delPer=0; H_PS_PSSID=28884_1434_21121_28774_28722_28557_28834_28585_28604; PSINO=2; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1555505635,1555661776,1555681721,1555681901; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1555681901; from_lang_often=%5B%7B%22value%22%3A%22spa%22%2C%22text%22%3A%22%u897F%u73ED%u7259%u8BED%22%7D%2C%7B%22value%22%3A%22dan%22%2C%22text%22%3A%22%u4E39%u9EA6%u8BED%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D",
-'User-Agent':"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
+'Cookie':"OUTFOX_SEARCH_USER_ID_NCOO=1890657925.3184712; OUTFOX_SEARCH_USER_ID=-725726515@10.168.8.64; JSESSIONID=aaarcwjTZ1PuAhwfog2Ow; ___rl__test__cookies=1555685768764",
+'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
+'Accept':"application / json, text / javascript, * / *; q = 0.01",
+'Accept - Language':"zh - CN",
+'Cache - Control':"no - cache",
+'Connection':"Keep-Alive",
+'Content-Length':"235",
+'Content-Type':"application/x-www-form-urlencoded; charset=UTF-8",
+'Host':"fanyi.youdao.com",
+'Origin':"http://fanyi.youdao.com",
+'Referer':"http://fanyi.youdao.com/",
+'X-Requested-With':"XMLHttpRequest",
 }
-str="yes"
-response=urllib.request.Request(url=url,headers=heades_data,data=urllib.parse.urlencode(get_post(str)).encode("gzip"))
-fanyi_data=urllib.request.urlopen(response)
-print(fanyi_data)
+str="Internal Server Error"
+#str=input()
+response=urllib.request.Request(url=url,headers=heades_data,data=urllib.parse.urlencode(get_post(str)).encode("utf-8"))
+fanyi_data=json.loads(urllib.request.urlopen(response).read().decode('utf-8'))
+pprint.pprint(fanyi_data)
+#pprint.pprint(fanyi_data['smartResult']['entries'])
+#pprint.pprint(fanyi_data['translateResult'][0][0]['tgt'])
