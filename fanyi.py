7276788b09adab2a7055b2822278fd6849016320
Administrator <2298440231@qq.com>
2019-04-20 11:02:46 +0800

    完成，该网站未做反爬比较近容易

--- fanyi.py
+++ fanyi.py
@@ -4,40 +4,30 @@ import urllib.parse
 import ssl
 import pprint
 ssl._create_default_https_context=ssl._create_unverified_context
-url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+url="http://fy.iciba.com/ajax.php?a=fy"
 def get_post(str):
     post={
-        'i':str,
-        'from':"en",
-        'to':"zh - CHS",
-        'smartresult':"dict",
-        'client':"fanyideskweb",
-        'salt':"15556871593500",#emmm
-        'sign':"10c32e10790706905bec2fe52eb36902",#weizhi  978456eb55df87ee03fb408f816633f5
-        'ts':"1555687135134",
-        'bv':"db6b9693e24ea152843dd96fcd289f0f",
-        'doctype':"json",
-        'version':"2.1",
-        'keyfrom':"fanyi.web",
-        'action':"FY_BY_CLICKBUTTION",
+        'f':"auto",
+        't':"auto",
+        'w':str,
     }
     return post
 heades_data={
-'Cookie':"OUTFOX_SEARCH_USER_ID_NCOO=1890657925.3184712; OUTFOX_SEARCH_USER_ID=-725726515@10.168.8.64; JSESSIONID=aaarcwjTZ1PuAhwfog2Ow; ___rl__test__cookies=1555685768764",
+'Cookie':"iciba_u_rand=6d8ba9bcfa60c4298123f4fafe8313ef%4027.155.93.79; iciba_u_rand_t=1555728876; UM_distinctid=16a38aabe2249b-058c9ab0f78e4e-3d644601-1fa400-16a38aabe23748; CNZZDATA1256573702=920922850-1555728091-http%253A%252F%252Fwww.iciba.com%252F%7C1555728091; __gads=ID=2d57dd182e5dfb12:T=1555728887:S=ALNI_MbwX44FipcaZfdsJuy_DjaOZFfb2A",
 'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
-'Accept':"application / json, text / javascript, * / *; q = 0.01",
-'Accept - Language':"zh - CN",
-'Cache - Control':"no - cache",
-'Connection':"Keep-Alive",
-'Content-Length':"235",
-'Content-Type':"application/x-www-form-urlencoded; charset=UTF-8",
-'Host':"fanyi.youdao.com",
-'Origin':"http://fanyi.youdao.com",
-'Referer':"http://fanyi.youdao.com/",
-'X-Requested-With':"XMLHttpRequest",
+# 'Accept':"application / json, text / javascript, * / *; q = 0.01",
+# 'Accept - Language':"zh - CN",
+# 'Cache - Control':"no - cache",
+# 'Connection':"Keep-Alive",
+# 'Content-Length':"235",
+# 'Content-Type':"application/x-www-form-urlencoded; charset=UTF-8",
+# 'Host':"fanyi.youdao.com",
+# 'Origin':"http://fanyi.youdao.com",
+# 'Referer':"http://fanyi.youdao.com/",
+# 'X-Requested-With':"XMLHttpRequest",
 }
-str="Internal Server Error"
-#str=input()
+str="yes"#"Internal Server Error"
+str=input()
 response=urllib.request.Request(url=url,headers=heades_data,data=urllib.parse.urlencode(get_post(str)).encode("utf-8"))
 fanyi_data=json.loads(urllib.request.urlopen(response).read().decode('utf-8'))
 pprint.pprint(fanyi_data)
