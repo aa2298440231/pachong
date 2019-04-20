1122e9d4ddb1d2bf2516e82e3dc0a886da9f9ef5
Administrator <2298440231@qq.com>
2019-04-20 20:03:25 +0800

    1.1 听云发送验证码线程类 cookie会变动

--- tinyunzhuche.py
+++ tinyunzhuche.py
@@ -1,29 +1,35 @@
 import urllib.request
 import urllib.parse
 import dama
+import threading
 import 代理ip
-yzm_url='http://member.tingyun.com/member/user/getImage?date=Sat%20Apr%2020%202019%2017:05:05%20GMT+0800%20(%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4)'
-dx_url='http://member.tingyun.com/member/regist/sendMoblieCode'
-head_data={
-'Cookie':"JSESSIONID=6400BD139A08671A5023D901A1553842; ty_member_uid=352c4a7e52bcae3bdad58ea9cfdb356d; SERVER_ID=279fc1c9-d3eeddd7",
-'Host':"member.tingyun.com",
-'Referer':"http://member.tingyun.com/member/regist",
-'User-Agent':"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
-}
-def get_post(yzm,iphon):
-    post_data={
-        'mobileNum':iphon,#13125183836
-        'codeCaptcha':yzm,
-        'type':"1",
+class tingyun(threading.Thread):
+    yzm_url='http://member.tingyun.com/member/user/getImage?date=Sat%20Apr%2020%202019%2017:05:05%20GMT+0800%20(%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4)'
+    dx_url='http://member.tingyun.com/member/regist/sendMoblieCode'
+    head_data={
+    'Cookie':"JSESSIONID=6D066A0ADD605A33918A89920F60A877; ty_member_uid=adcde6b43ddd3710d4e0b3cf95bfd918; SERVER_ID=279fc1c9-d3eeddd7",
+    'Host':"member.tingyun.com",
+    'Referer':"http://member.tingyun.com/member/regist",
+    'User-Agent':"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
     }
-    return post_data
-def main(iphon):
-    yzm_response=urllib.request.Request(url=yzm_url,headers=head_data)
-    with open("yzm.png","wb") as f:
-        f.write(urllib.request.urlopen(yzm_response).read())
-    yzm=dama.get_code_text(file_name='yzm')
-    dx_respones=urllib.request.Request(url=dx_url,headers=head_data,data=urllib.parse.urlencode(get_post(yzm=yzm,iphon=iphon)).encode('utf-8'))
-    print(urllib.request.urlopen(dx_respones).read().decode("utf-8"))
+    def get_post(yzm,iphon):
+        post_data={
+            'mobileNum':iphon,#13125183836
+            'codeCaptcha':yzm,
+            'type':"1",
+        }
+        return post_data
+    def __init__(self,iphon):
+        threading.Thread.__init__(self)
+        self.iphon=iphon
+    def run(self):
+        yzm_response=urllib.request.Request(url=tingyun.yzm_url,headers=tingyun.head_data)
+        with open("yzm.png","wb") as f:
+            f.write(urllib.request.urlopen(yzm_response).read())
+        yzm=dama.get_code_text(file_name='yzm')
+        dx_respones=urllib.request.Request(url=tingyun.dx_url,headers=tingyun.head_data,data=urllib.parse.urlencode(tingyun.get_post(yzm=yzm,iphon=self.iphon)).encode('utf-8'))
+        print(urllib.request.urlopen(dx_respones).read().decode("utf-8"))
 
 代理ip.dailiip()
-main('13125183836')
+t=tingyun('13343509055')#13125183836
+t.start()
